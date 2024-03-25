4db432243acbc9b752c74574800077ce9acc6abb
thangyou <yangyujeong4@gmail.com>
2024-03-25 21:26:46 +0900

    initial commit

---
+++ src/main/java/com/newzone/apikorail/ApiKorailApplication.java
@@ -0,0 +1,13 @@
+package com.newzone.apikorail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKorailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKorailApplication.class, args);
+	}
+
+}
