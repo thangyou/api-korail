8228a7153788c4dc0049b5e321cff528c5748e85
thangyou <yangyujeong4@gmail.com>
2024-04-23 16:55:00 +0900

    [Refactor] 중복 코드 수정

--- src/main/java/com/newzone/apikorail/Train/service/TrainService.java
+++ src/main/java/com/newzone/apikorail/Train/service/TrainService.java
@@ -29,6 +29,30 @@ public class TrainService {
 
 
     // ================================================================================================================
+    public String callExternalApi(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb); // 로그 확인용
+        return sb.toString();
+    }
+
+
     /**
      * 출/도착지 기반 열차 정보 조회
      */
@@ -42,30 +66,11 @@ public class TrainService {
         urlBuilder.append("&" + URLEncoder.encode("arrPlaceId","UTF-8") + "=" + URLEncoder.encode(trainRequest.arrivalPlaceId, "UTF-8")); /*도착기차역ID [상세기능3. 시/도별 기차역 목록조회]에서 조회 가능*/
         urlBuilder.append("&" + URLEncoder.encode("depPlandTime","UTF-8") + "=" + URLEncoder.encode(trainRequest.departureDate, "UTF-8")); /*출발일(YYYYMMDD)*/
         urlBuilder.append("&" + URLEncoder.encode("trainGradeCode","UTF-8") + "=" + URLEncoder.encode(trainRequest.trainGradeCode, "UTF-8")); /*차량종류코드*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        System.out.println(sb.toString()); // 로그 확인용
 
-        // 응답 결과를 TrainResponse 객체 리스트로 변환
+        String apiResponse = callExternalApi(urlBuilder.toString());
+
         List<TrainResponse> trainResponseList = new ArrayList<>();
-        // Parse JSON response and create TrainResponse objects
-        JsonNode rootNode = objectMapper.readTree(sb.toString());
+        JsonNode rootNode = objectMapper.readTree(apiResponse);
         JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
         for (JsonNode itemNode : itemsNode) {
             TrainResponse trainResponse = new TrainResponse();
@@ -91,28 +96,12 @@ public class TrainService {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/TrainInfoService/getVhcleKndList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 타입(xml, json)*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        System.out.println(sb); // 로그 확인용
+
+        String apiResponse = callExternalApi(urlBuilder.toString());
+
 
         List<KindResponse> kindResponseList = new ArrayList<>();
-        JsonNode rootNode = objectMapper.readTree(sb.toString());
+        JsonNode rootNode = objectMapper.readTree(apiResponse);
         JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
             for (JsonNode itemNode : itemsNode) {
                 KindResponse kindResponse = new KindResponse();
@@ -169,28 +158,11 @@ public class TrainService {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/TrainInfoService/getCtyCodeList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 타입(xml, json)*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        System.out.println(sb); // 로그 확인용
+
+        String apiResponse = callExternalApi(urlBuilder.toString());
 
         List<CityCodeResponse> cityCodeList = new ArrayList<>();
-        JsonNode rootNode = objectMapper.readTree(sb.toString());
+        JsonNode rootNode = objectMapper.readTree(apiResponse);
         JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
         for (JsonNode itemNode : itemsNode) {
             CityCodeResponse cityCodeResponse = new CityCodeResponse();
@@ -234,28 +206,11 @@ public class TrainService {
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(String.valueOf(request.numOfRows), "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 타입(xml, json)*/
         urlBuilder.append("&" + URLEncoder.encode("cityCode","UTF-8") + "=" + URLEncoder.encode(request.cityCode, "UTF-8")); /*시/도 ID*/
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        System.out.println(sb); // 로그 확인용
+
+        String apiResponse = callExternalApi(urlBuilder.toString());
 
         List<CtyAcctoTrainSttnResponse> CtyAcctoTrainSttnList = new ArrayList<>();
-        JsonNode rootNode = objectMapper.readTree(sb.toString());
+        JsonNode rootNode = objectMapper.readTree(apiResponse);
         JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
         for (JsonNode itemNode : itemsNode) {
             CtyAcctoTrainSttnResponse ctyAcctoTrainSttnResponse = new CtyAcctoTrainSttnResponse();
